24cdbf6337c9cd3d28a57111120b85d6cbdd4c47
HaullySimas <HaullySimas@users.noreply.github.com>
2017-12-11 07:11:23 -0500

    Working on ScienceDeploy

---
+++ src/Deploy/ScienceDeploy.cs
@@ -0,0 +1,19 @@
+﻿namespace KERBALISM
+{
+  public class ScienceDeploy : DeployBase
+  {
+    // List of target
+    // Part name: 
+    //    OrbitalScanner
+    //    SurveyScanner
+    //    InfraredTelescope
+    //    GooExperiment
+    //    science_module (science.module)
+    public override bool IsActive => throw new System.NotImplementedException();
+
+    public override void Start()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
